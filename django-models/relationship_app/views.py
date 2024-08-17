23ed76251a8910bae89d83ddcc8c07605d0e2f67
Ouko Okowa <oukopinto@gmail.com>
2024-08-17 20:55:32 +0200

    Added login, logout, and registration authentication

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -11,7 +11,7 @@ from django.shortcuts import redirect
 # Create your views here.
 
 class RegisterView(CreateView):
-    form_class = UserCreationForm
+    form_class = UserCreationForm()
     template_name = 'relationship_app/register.html'
     success_url = reverse_lazy('login')
 
