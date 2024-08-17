ad9a2d95655a76f18b508882dd147362a07310ed
Ouko Okowa <oukopinto@gmail.com>
2024-08-17 20:48:36 +0200

    Added login, logout, and registration authentication

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -2,8 +2,24 @@ from django.shortcuts import render
 from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.shortcuts import redirect
 
 # Create your views here.
+
+class RegisterView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'relationship_app/register.html'
+    success_url = reverse_lazy('login')
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('home')
+
 def list_books(request):
     books = Book.objects.all()
     context = {'list_books': books}
