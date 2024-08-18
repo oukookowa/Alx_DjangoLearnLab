2ec6c9f328e2be3c30fa3e14622a6b42cda9a22e
Ouko Okowa <oukopinto@gmail.com>
2024-08-18 14:34:45 +0200

    Added login, logout, and registration authentication

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -6,10 +6,10 @@ from . import views
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(template_name='relationship_app/library_detail.html'), name='library_detail'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-    path('register/', RegisterView.as_view(template_name='relationship_app/register.html'), name='register'),
+    path('register/', RegisterView.as_view(template_name='relationship_app/register.html'), name='views.register'),
     path('admin/', views.admin_view, name='admin_view'),
     path('librarian/', views.librarian_view, name='librarian_view'),
     path('member/', views.member_view, name='member_view'),
