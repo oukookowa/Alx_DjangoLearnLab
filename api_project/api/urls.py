0ecf4553ea6f1397e22cb99849c36271e474640b
Ouko Okowa <oukopinto@gmail.com>
2024-08-29 13:22:19 +0200

    Added a BookViewSet that provides CRUD operations on the serialized Book model

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path
-from .views import BookList  # Import the view
+from django.urls import path, include
+from .views import BookViewSet  # Import the view
+from rest_framework.routers import DefaultRouter
 
+router = DefaultRouter()
+router.register(r'books', BookViewSet)
 urlpatterns = [
-    path('books/', BookList.as_view(), name='book-list'),  # Define the URL pattern for the view
+    #path('books/', BookList.as_view(), name='book-list'),  # Define the URL pattern for the view
+    path('api/', include(router.urls)),
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,7 +1,11 @@
-from rest_framework import generics
+from rest_framework import viewsets #generics
 from .models import Book  # Import your Book model
 from .serializers import BookSerializer  # Import the serializer you just created
 
-class BookList(generics.ListAPIView):
-    queryset = Book.objects.all()  # Define the queryset to return all Book instances
-    serializer_class = BookSerializer  # Use the BookSerializer to serialize the data
+#class BookList(generics.ListAPIView):
+    #queryset = Book.objects.all()  # Define the queryset to return all Book instances
+    #serializer_class = BookSerializer  # Use the BookSerializer to serialize the data
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
