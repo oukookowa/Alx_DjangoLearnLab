06fd72dcf1403a9fb38fed6b3124cd302d6953a9
Ouko Okowa <oukopinto@gmail.com>
2024-09-07 19:34:33 +0200

    creaed api models and views for the api app and mapped them to respective urls

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from .views import BookListView, BookDetailView, BookCreateView, BookUpdateView, BookDeleteView
 
-# configuring the path for each of the api views
+# Configuring the path for each of the api views
 urlspatterns = [
     path('books/', BookListView.as_view(), name='book-list'),
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
     path('books/create/', BookCreateView.as_view(), name='book-create'),
-    path('books/<int:pk>/update', BookUpdateView.as_view(), name='book-update'),
-    path('books/<int:pk>/delete', BookDeleteView.as_view(), name='book-delete'),    
+    path('books/update/<int:pk>/', BookUpdateView.as_view(), name='book-update'),
+    path('books/delete/<int:pk>/', BookDeleteView.as_view(), name='book-delete'),    
 ]
