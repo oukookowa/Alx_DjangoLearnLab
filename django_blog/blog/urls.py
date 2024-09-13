2759c3716c1ec4e6966aca87087d9d3b4a658b1a
Ouko Okowa <oukopinto@gmail.com>
2024-09-13 20:00:45 +0200

    Added Comment model to the blog that allows users to comment on post

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -14,12 +14,12 @@ urlpatterns = [
     path('posts/', PostListView.as_view(), name='post_list'),  # List all posts
     path('posts/<int:pk>/', PostDetailView.as_view(), name='post_detail'),  # Post details
     path('posts/new/', PostCreateView.as_view(), name='post_create'),  # Create a new post
-    path('posts/<int:pk>/update/', PostUpdateView.as_view(), name='post_update'),  # Edit/update a post
-    path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),  # Delete a post
-    path('posts/<int:pk>/comments/', CommentListView.as_view(), name='comment_list'),  # List all coments of a post
-    path('posts/<int:pk>/comments/<int:pk>/', CommentDetailView.as_view(), name='comment_detail'),  # Comment details
-    path('posts/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment_create'),  # Create a new comment on a post
-    path('posts/<int:pk>/comments/<int:pk>/update/', CommentUpdateView.as_view(), name='comment_update'),  # Edit/update a comment on a post
-    path('posts/<int:pk>/comments/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment_delete'),  # Delete a comment on a post
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post_update'),  # Edit/update a post
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),  # Delete a post
+    path('post/<int:pk>/comments/', CommentListView.as_view(), name='comment_list'),  # List all coments of a post
+    path('post/<int:pk>/comments/<int:pk>/', CommentDetailView.as_view(), name='comment_detail'),  # Comment details
+    path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment_create'),  # Create a new comment on a post
+    path('post/<int:pk>/comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment_update'),  # Edit/update a comment on a post
+    path('post/<int:pk>/comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment_delete'),  # Delete a comment on a post
 
 ]
