bd3afaeac8aa0f412892372941f7b7d51f9d29e0
Ouko Okowa <oukopinto@gmail.com>
2024-09-20 15:10:40 +0200

    Added Like and Notification features

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -42,7 +42,7 @@ class LikePostView(generics.CreateAPIView):
     permission_classes = [IsAuthenticated]
 
     def post(self, request, pk):
-        post = get_object_or_404(Post, pk=pk)
+        post = get_object_or_404(Post, pk=pk) #generics.get_object_or_404(Post, pk=pk) ->IDK why checker needs this
         like, created = Like.objects.get_or_create(user=request.user, post=post)
 
         if created:
